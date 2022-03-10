79f3ebb30739b88797f1b9ceef55aff033c0d2ee
LeandroRocha <leandro@eulabs.com.br>
2022-03-09 22:09:47 -0400

    logo

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todolist/app/modules/auth/login/login_controller.dart';
+import 'package:todolist/app/core/widget/todo_list_logo.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Provider.of<LoginController>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+                minWidth: constraints.maxWidth,
+              ),
+              child: IntrinsicHeight(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(height: 10),
+                    TodoListLogo(),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
       ),
-      body: Container(),
     );
   }
 }
